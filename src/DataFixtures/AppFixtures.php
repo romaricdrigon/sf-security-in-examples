de77ad0deca5ed5643d23d941c337b86ba308c30
Romaric Drigon <romaric.drigon@gmail.com>
2019-08-28 11:07:14 +0200

    Added DB migration and data fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Article;
+use App\Entity\Blog;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,8 +11,47 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $webBlog = (new Blog())
+            ->setName('Web Summer Camp official blog')
+        ;
+
+        $manager->persist($webBlog);
+
+        $welcomeArticle = (new Article())
+            ->setTitle('Welcome to WSC 2019!')
+            ->setContent('Ever since we started this event in 2012, the main concept hasn’t changed: '
+                .'hands-on workshops held by experts in a relaxing environment, '
+                .'a chance to go deeper with a topic than you can in a regular conference...')
+            ->setBlog($webBlog)
+        ;
+
+        $manager->persist($welcomeArticle);
+
+        $rdBlog = (new Blog())
+            ->setName('Romaric Drigon\'s blog');
+
+        $manager->persist($rdBlog);
+
+        $helloWorld = (new Article())
+            ->setTitle('Hello World!')
+            ->setContent('Welcome to my blog! After blogging for a while on https://blog.netinfluence.ch, '
+                .'in French, I wanted to start writing in English too. Hence this blog! '
+                .'I will continue publishing articles on both, though. They will have different topics and approaches. '
+                .'Here, I will try to dig into more advanced Symfony topics...')
+            ->setBlog($rdBlog)
+        ;
+
+        $manager->persist($helloWorld);
+
+        $doctrine = (new Article())
+            ->setTitle('About Doctrine inheritance')
+            ->setContent('At a previous SymfonyLive conference (slides here, in French), '
+                .'I advised not to use Doctrine inheritance mapping. After a discussion with another developer last week, '
+                .'I realized the subject is worth an article with more details...')
+            ->setBlog($rdBlog)
+        ;
+
+        $manager->persist($doctrine);
 
         $manager->flush();
     }
---
+++ src/Migrations/Version20190828085500.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20190828085500 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return 'Added basic Blog/Article schema';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE blog (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE article (id INT AUTO_INCREMENT NOT NULL, blog_id INT NOT NULL, title VARCHAR(255) NOT NULL, content LONGTEXT DEFAULT NULL, INDEX IDX_23A0E66DAE07E97 (blog_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE article ADD CONSTRAINT FK_23A0E66DAE07E97 FOREIGN KEY (blog_id) REFERENCES blog (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE article DROP FOREIGN KEY FK_23A0E66DAE07E97');
+        $this->addSql('DROP TABLE blog');
+        $this->addSql('DROP TABLE article');
+    }
+}
